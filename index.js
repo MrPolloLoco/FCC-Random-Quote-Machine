a4f9443992d1c4988cdf76881f7b6be894e43fd5
Kloud <theateamdev@gmail.com>
2023-10-03 13:50:04 -0700

    Added import syntax

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./src/App";
 import "./CSS/index.css";
+import { Analytics } from '@vercel/analytics/react';
 
 reactDom.render(
 <App />,
+<Analytics />,
 document.getElementById("root"));
